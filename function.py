6f4ade14f1b5586d97adeae8da9ae0b4c83972c7
S-S-Belousov <108421600+S-S-Belousov@users.noreply.github.com>
2022-09-29 16:48:58 +0300

    Add files via upload

--- function.py
+++ function.py
@@ -47,3 +47,16 @@ def getbinarynumber(number):
         binarynumber += str(number % 2)
         number //= 2
     return binarynumber
+
+
+def get_fibonacci(n):
+    fibo_nums = []
+    a, b = 1, 1
+    for i in range(n):
+        fibo_nums.append(a)
+        a, b = b, a + b
+    a, b = 0, 1
+    for i in range(n+1):
+        fibo_nums.insert(0, a)
+        a, b = b, a - b
+    return fibo_nums
---
+++ task5.py
@@ -0,0 +1,6 @@
+# Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
+from function import get_fibonacci
+
+number = int(input("Введите целое число: "))
+
+print(get_fibonacci(number))
